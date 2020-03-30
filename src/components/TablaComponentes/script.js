e7e827f38d52428942233095e17a9e1b92cf48e2
MRodrig7 <marcosrodrig7@gmail.com>
2020-03-30 19:17:59 +0200

    Componente TablaComponentes

---
+++ src/components/TablaComponentes/script.js
@@ -0,0 +1,19 @@
+export default {
+	name: 'TablaComponentes',
+	props: {
+		title: {
+			type: String,
+			default: 'Holaaa'
+		},
+		header: {
+			type: Array
+		},
+		date: {
+			type: Array
+		}
+	},
+	computed: {
+	},
+	methods: {
+	}
+}
