679852ab73ccb20e70f9f9325bea1bf38d1bde00
danikage <samurai.dani@gmail.com>
2020-03-30 21:15:38 +0200

    change component to props

--- src/components/MenuNav/script.js
+++ src/components/MenuNav/script.js
@@ -1,5 +1,13 @@
 export default {
 	name: 'MenuNav',
+	props: {
+		links: {
+			type: Array,
+			default: function(){
+				return []
+			}
+		}
+	},
 	computed: {
 		showMenu(){
 			return this.$store.state.menuNav.display
