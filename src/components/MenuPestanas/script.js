25d65fa6a376f35a24aa49be8f194eb4c2143f40
danikage <samurai.dani@gmail.com>
2020-04-01 21:51:06 +0200

    simple idea to continue

--- src/components/MenuPestanas/script.js
+++ src/components/MenuPestanas/script.js
@@ -1,18 +1,38 @@
 export default {
 	name: 'MenuPestanas',
-	template: '<slot v-for="dia in dias" :nombre="dia">{{ dia }}</slot>',
+	// template: '<slot v-for="dia in dias" :nombre="dia">{{ dia }}</slot>',
 	props: {
 		item:{
 			type: Array
 		}
+		// headers: ....lo que sea
 	},
 	data() {
 		return {
+			nombrefeoclaseactiva1: '',
+			nombrefeoclaseactiva2: '',
+			nombrefeoclaseactiva3: '',
 			dia: ['lunes', 'martes']
 		}
 	},
 	computed: {
 	},
 	methods: {
+		funcionconnombrefeo(tabClicked){
+			console.log('fe')
+			// para no pensar, primero reseteo las clases
+			this.nombrefeoclaseactiva1 = '';
+			this.nombrefeoclaseactiva2 = '';
+			this.nombrefeoclaseactiva3 = '';
+
+			// y activo la que debería:
+			if(tabClicked == 1){
+				this.nombrefeoclaseactiva1 = 'menu-pestanas__nombre-feo--active';
+			} else if(tabClicked == 2){
+				this.nombrefeoclaseactiva2 = 'menu-pestanas__nombre-feo--active';
+			} else {
+				this.nombrefeoclaseactiva3 = 'menu-pestanas__nombre-feo--active';
+			}
+		}
 	}
 }
--- src/views/Test/script.js
+++ src/views/Test/script.js
@@ -1,12 +1,8 @@
-import HelloWorld from '@/components/HelloWorld.vue'
-import TablaComponentes from '@/components/TablaComponentes/index.vue'
 import MenuPestanas from '@/components/MenuPestanas/index.vue'
 
 export default {
-  name: 'Test',
-  components: {	
-    HelloWorld,
-    TablaComponentes,
-    MenuPestanas
-  }
+	name: 'Test',
+	components: {
+		MenuPestanas
+	}
 }
