3f70fcc66bd4892d265833d1f1074f37f023d40a
MRodrig7 <marcosrodrig7@gmail.com>
2020-04-01 20:54:50 +0200

    Vista test con los ejemplos tabla y menupestañas

--- src/views/Test/script.js
+++ src/views/Test/script.js
@@ -1,8 +1,12 @@
 import HelloWorld from '@/components/HelloWorld.vue'
+import TablaComponentes from '@/components/TablaComponentes/index.vue'
+import MenuPestanas from '@/components/MenuPestanas/index.vue'
 
 export default {
   name: 'Test',
-  components: {
-    HelloWorld
+  components: {	
+    HelloWorld,
+    TablaComponentes,
+    MenuPestanas
   }
 }
