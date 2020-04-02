76d1996ce22bb78d033cfad96b471d8926343d56
MRodrig7 <marcosrodrig7@gmail.com>
2020-04-02 11:46:18 +0200

    Creacion del componente minicard

---
+++ src/components/MiniCard/script.js
@@ -0,0 +1,21 @@
+export default {
+	name: 'MiniCard',
+	props: {
+		img:{
+			type: String,
+			default: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png'
+		},
+		name:{
+			type: String,
+			default: 'Name'
+		}
+	},
+	data() {
+		return {
+		}
+	},
+	computed: {
+	},
+	methods: {
+	}
+}
--- src/views/Test/script.js
+++ src/views/Test/script.js
@@ -1,8 +1,10 @@
 import MenuPestanas from '@/components/MenuPestanas/index.vue'
+import MiniCard from '@/components/MiniCard/index.vue'
 
 export default {
 	name: 'Test',
 	components: {
-		MenuPestanas
+		MenuPestanas,
+		MiniCard
 	}
 }
