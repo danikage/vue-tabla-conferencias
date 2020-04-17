c209387db4d26084638872a38421de6c15971479
MRodrig7 <marcosrodrig7@gmail.com>
2020-04-17 17:48:00 +0200

    Correcion vista peopleDetail

--- src/views/PeopleDetail/script.js
+++ src/views/PeopleDetail/script.js
@@ -1,13 +1,20 @@
+import CardPersona from '@/components/CardPersona/index.vue'
+
 export default {
 	name: 'PeopleDetail',
 	components: {
+		CardPersona,
 	},
 	data: () => ({
 		peopleId: null,
 	}),
 	computed: {
+		speakerDetail(){
+			return this.$store.state.speakerDetail;
+		},
 	},
 	created(){
 		this.peopleId = this.$route.params.peopleId;
+		this.$store.dispatch('getSpeakerDetail', this.peopleId);
 	}
 }
