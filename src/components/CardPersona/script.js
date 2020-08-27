296a918e169641d1c7a8681005bd2ebfac115b3d
danikage <samurai.dani@gmail.com>
2020-08-27 22:09:51 +0200

    image fixed

--- src/components/CardPersona/script.js
+++ src/components/CardPersona/script.js
@@ -25,15 +25,17 @@ export default {
 		},
 	},
 	data: () => ({
-		image: '',
 		image_default: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png',
 	}),
-	created() {
-		this.image = this.img != '' ? require('@/assets/'+this.img) : this.image_default
-	},
 	methods:{
 		getColor(digit){
 			return 'card-persona--type-' + digit
-		}
+		},
+		getImgProfile(img){
+			return this.img != '' ? require('@/assets/' + img) : this.image_default
+		},
+		getImgRrss(img){
+			return require('@/assets/' + img)
+		},
 	},
 }
